d7bde81527ea30fa950198952fbe5fdaefb2f728
Clemence Ayekple <clemence.ayekple@ucc.edu.gh>
2020-08-01 08:10:27 +0000

    initial navigation drawer

--- App.js
+++ App.js
@@ -2,20 +2,29 @@ import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 // import Main from './src/Main';
 
 // Screens
 import HomeScreen from './src/screens/HomeScreen';
+import DetailScreen from './src/screens/DetailScreen';
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const DetailStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Details" component={DetailScreen} />
+        </Drawer.Navigator>
+        {/* <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailScreen} />
+        </Stack.Navigator> */}
       </NavigationContainer>
     </PaperProvider>
   );
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to details screen"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 };
